2aff457751093b14ac3df7ab8def7d47ea42103f
Abhiwebcraft <abhi200maykumar@gmail.com>
2024-06-28 14:36:00 +0530

    AI Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from './assets/Components/Main/Main'
+import Sidebar from './assets/Components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <>
+    <Sidebar />
+    <Main />
+    </>
+  )
+}
+
+export default App
